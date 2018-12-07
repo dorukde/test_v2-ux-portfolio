1ad50473adf8143292376ec35f7e56d88b060e48
dorukde <dorukde@gmail.com>
2018-12-07 15:44:06 +1100

    gatsby-image-fixed

--- src/pages/streetkak.js
+++ src/pages/streetkak.js
@@ -9,6 +9,9 @@ import HText1 from '../components/text1'
 import Text2 from '../components/text2'
 import SHeader from '../components/sheader'
 
+/*STYLED COMPONENT */
+import styled from 'styled-components'
+
 const skakPage = props => (
   <div id="HeroAbout2" className="HeroAbout">
     <div className="AboutW">
@@ -53,14 +56,30 @@ const skakPage = props => (
         <div className="SectionTitle">Introduction</div>
         <HText1
           Header1="Street-food in Scandinavia"
-          Text1="Summer of 2014, marked the launch of street-food trucks in Stockholm, and made a big impact during the short summer (e.g. a new TV-series about street-food). Streetkäk (swe: street + shack) started as a passion project, including an ex hamburger-kiosk chef. Initial idea of Streetkäk has been locating and finding food-trucks in the city before they were out in the wild. The time was scarce for street-food lovers, as well as the limited supply of good-taste to be chased."
+          Text1="Summer of 2014, marked the launch of street-food trucks in Stockholm, and made a big impact during the short summer (e.g. a new TV-series about street-food). Streetkäk (street + snack) started as a passion project, including an ex hamburger-kiosk chef. Initial idea of Streetkäk has been locating and finding food-trucks in the city before they were out in the wild. The time was scarce for street-food lovers, as well as the limited supply of good-taste to be chased."
         />
         <Text2 Text2="Streetkäk's visual language has been crafty, humanistic, with a team having a complete 'DIY' approach. After having collaborated with the food-truck chefs and observing their interaction with the community, the visal language has shaped as  crafty, custom and 'humanist', as tech-savvy as greasy hands and a hungry mind." />
         <br />
-        <Img fluid={props.data.skOne.childImageSharp.fluid} />
-        <Img fluid={props.data.skTwo.childImageSharp.fluid} />
-        <Img fluid={props.data.skThree.childImageSharp.fluid} />
-        <Img fluid={props.data.skFour.childImageSharp.fluid} />
+        <Img
+          fixed={props.data.skOne.childImageSharp.fixed}
+          alt="Logo and App Icon"
+          style={{ maxWidth: 400 }}
+        />
+        <Img
+          fixed={props.data.skTwo.childImageSharp.fixed}
+          alt="Typography and Colour"
+          style={{ maxWidth: 400 }}
+        />
+        <Img
+          fixed={props.data.skThree.childImageSharp.fixed}
+          alt="Iconography"
+          style={{ maxWidth: 400 }}
+        />
+        <Img
+          fixed={props.data.skFour.childImageSharp.fixed}
+          alt="State Indicators"
+          style={{ maxWidth: 400 }}
+        />
         <HText1
           Header1="The Problem"
           Text1="'Communication' is at the heart of street-food culture. Street-food can be thought as a storytelling process, involving the chef, the story of the food, and participation of street-food lover. In Nordic cities, there are regulations about time-interval, location and schedule of sales for food trucks. Communication within the street-food community depended on 'happy coincidences' and social media, without a specific platform. Often times, food-truck chefs run a 'one man show', making it harder to manage social-media updates while cooking at the same time. "
@@ -75,7 +94,10 @@ const skakPage = props => (
           Header1="Persona of a Food-Truck"
           Text1="'Communication' is the core concept shaping street-food culture. It is a complete-storytelling process that is personal, involving the chef, the story of the food, and participation of street-food lover. However, the trucks needed a platform for being able to say 'Hi' and introduce their menu/prices for making the most of their limited time on the street. Streetkäk provided an admin-panel where food-truck owners could add/edit their intro/menu, add food specifications (happy cows &amp; no more allergies)."
         />
-        <Img fluid={props.data.skFive.childImageSharp.fluid} />
+        <Img
+          fluid={props.data.skFive.childImageSharp.fluid}
+          alt="Personalising Trucks"
+        />
         <div className="SectionTitle">Streetkäk Check-in</div>
         <HText1
           Header1="The Process"
@@ -107,20 +129,29 @@ export const fluidImage2 = graphql`
     }
   }
 `
+export const fixedImage = graphql`
+  fragment fixedImage on File {
+    childImageSharp {
+      fixed(quality: 100, width: 400) {
+        ...GatsbyImageSharpFixed
+      }
+    }
+  }
+`
 
 export const pageQuery = graphql`
   query {
     skOne: file(relativePath: { eq: "sk1.jpg" }) {
-      ...fluidImage2
+      ...fixedImage
     }
     skTwo: file(relativePath: { eq: "sk2.jpg" }) {
-      ...fluidImage2
+      ...fixedImage
     }
     skThree: file(relativePath: { eq: "sk3.jpg" }) {
-      ...fluidImage2
+      ...fixedImage
     }
     skFour: file(relativePath: { eq: "sk4.jpg" }) {
-      ...fluidImage2
+      ...fixedImage
     }
     skFive: file(relativePath: { eq: "sk5.jpg" }) {
       ...fluidImage
