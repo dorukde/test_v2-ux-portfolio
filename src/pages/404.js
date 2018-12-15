cc77ee901e6dd1dc2f847c4eaa76a1a9235d97ff
dorukde <dorukde@gmail.com>
2018-12-16 02:40:41 +1100

    keyflowProto2

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,7 +5,7 @@ const NotFoundPage = () => (
     <div className="Content">
       <br />
       <br />
-      <h1>Hello!</h1>
+      <p>Hello!</p>
       <p>
         This is the Lionel you're looking for! You have just found a page that I
         have not designed yet. <br />
